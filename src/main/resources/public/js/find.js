ec72e5dcc7ce07eff5dd60b59f426c5db32e29ed
zhonghz <huz25@pitt.edu>
2016-03-30 01:11:47 -0400

    Update

--- src/main/resources/public/js/find.js
+++ src/main/resources/public/js/find.js
@@ -16,19 +16,8 @@ $(function () {
                                 '<h4>Username: '+member[i].username+'</h4>'+
                                 '<br/>'+'<p>Native Language: '+member[i].nlanguage+'</p>'+
                                 '<p>Practise Language: '+member[i].planguage+'</p>'+'<span class="time">Last Login Date</span>'+'</div>');
-
-          // $("div#members").html("<div class="demo"><img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" /><h4>Username</h4><br/><p>Native Language</p><p>Practise Language</p><span class="time">Last Login Date</span></div>");
         }
       }
-      //$("#current-time").html(result);
+
     }});
   });
-
-//DOM
-
-// $img = $("<img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" />");
-// $oh4 = $("<h4>Username+car[0]</h4>");
-// $br = $("<br/>");
-// $onlanguage = $("<p>Native Language</p>");
-// $oplanguage = $("<p>Practise Language</p>");
-// $otime = $("<span class="time">Last Login Date</span>");
