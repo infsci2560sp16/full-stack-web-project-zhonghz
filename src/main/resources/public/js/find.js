024fc6ae25bf6a587ec8504bef848f18f833898d
zhonghz <huz25@pitt.edu>
2016-03-30 00:09:30 -0400

    Update Get

--- src/main/resources/public/js/find.js
+++ src/main/resources/public/js/find.js
@@ -4,26 +4,35 @@ $(function () {
     success: function (result) {
       var member = JSON.parse(result);
       console.log(data);
-      for (var i = 0; i < members.length; i++) {
+      for (var i = 0; i < member.length; i++) {
         if (i%2 !== 0) {
-          $odddiv = $("<div class=‘edemo’></div>");
-          $("div#members").append($odddiv);
-          $($odddiv).append("<img class='userPic' src='images/userpic.jpg' alt='userpic' width='60' height='60' />");
-          $($odddiv).append("<h4>member[i].username</h4>");
-          $($odddiv).append("<br/>");
-          $($odddiv).append("<p>member[i].nlanguage</p>");
-          $($odddiv).append("<p>member[i].planguage</p>");
-          $($odddiv).append("<span class='time'>Last Login Date</span>");
-          // $("div#members").html("<div class="edemo"><img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" /><h4>Username+car[0]</h4><br/><p>Native Language</p><p>Practise Language</p><span class="time">Last Login Date</span></div>");
+          // $odddiv = $('<div class="edemo"></div>');
+          // $("div#members").append($odddiv);
+          // $($odddiv).append('<img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" />');
+          // $($odddiv).append("<h4>member[i].username</h4>");
+          // $($odddiv).append("<br/>");
+          // $($odddiv).append("<p>member[i].nlanguage</p>");
+          // $($odddiv).append("<p>member[i].planguage</p>");
+          // $($odddiv).append("<span class='time'>Last Login Date</span>");
+          $("div#members").html('<div class="edemo">'+
+                                '<img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" />'+
+                                '<h4>Username'+member[i].username+'</h4>'+
+                                '<br/>'+'<p>Native Language'+member[i].nlanguage+'</p>'+
+                                '<p>Practise Language'+member[i].planguage+'</p>'+'<span class="time">Last Login Date</span>'+'</div>');
         } else {
-          $evendiv = $("<div class='demo'></div>");
-          $("div#members").append($evendiv);
-          $($evendiv).append("<img class='userPic' src='images/userpic.jpg' alt='userpic' width='60' height='60' />");
-          $($evendiv).append("<h4>Username+car[0]</h4>");
-          $($evendiv).append("<br/>");
-          $($evendiv).append("<p>Native Language</p>");
-          $($evendiv).append("<p>Practise Language</p>");
-          $($evendiv).append("<span class='time'>Last Login Date</span>");
+          $("div#members").html('<div class="demo">'+
+                                '<img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" />'+
+                                '<h4>Username'+member[i].username+'</h4>'+
+                                '<br/>'+'<p>Native Language'+member[i].nlanguage+'</p>'+
+                                '<p>Practise Language'+member[i].planguage+'</p>'+'<span class="time">Last Login Date</span>'+'</div>');
+          // $evendiv = $("<div class='demo'></div>");
+          // $("div#members").append($evendiv);
+          // $($evendiv).append("<img class='userPic' src='images/userpic.jpg' alt='userpic' width='60' height='60' />");
+          // $($evendiv).append("<h4>Username+car[0]</h4>");
+          // $($evendiv).append("<br/>");
+          // $($evendiv).append("<p>Native Language</p>");
+          // $($evendiv).append("<p>Practise Language</p>");
+          // $($evendiv).append("<span class='time'>Last Login Date</span>");
           // $("div#members").html("<div class="edemo"><img class="userPic
           // $("div#members").html("<div class="demo"><img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" /><h4>Username</h4><br/><p>Native Language</p><p>Practise Language</p><span class="time">Last Login Date</span></div>");
         }
