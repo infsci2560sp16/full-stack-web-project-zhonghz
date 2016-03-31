4e656df67dca0628954ec29a8011f4b52f6888d4
zhonghz <huz25@pitt.edu>
2016-03-31 15:18:48 -0400

    Update XML

--- src/main/resources/public/js/forum.js
+++ src/main/resources/public/js/forum.js
@@ -11,10 +11,10 @@ function browseThread () {
 			for ( var i = 0; i < threads.length; i++) {
         $("div#demos").append('<div class="edemo" id="demo1">'+
         													'<img class="userPic" src="images/userpic.jpg" alt="userpic" width="60" height="60" />'+
-        		    									'<h4>'+'<a href="#">'+threads[i].childNodes[0].firstChild.nodeValue+'</a></h4>'+
-        													'<p>'+threads[i].childNodes[1].firstChild.nodeValue+'|'+threads[i].childNodes[2].firstChild.nodeValue+'</p>'+
-        	        								'<p class="description">'+threads[i].childNodes[4].firstChild.nodeValue+'</p>'+
-        													'<span class="time">'+threads[i].childNodes[3].firstChild.nodeValue+'</span>'+'</div>');
+        		    									'<h4>Title:'+'<a href="#">'+threads[i].childNodes[0].firstChild.nodeValue+'</a></h4>'+
+        													'<p>Username:'+threads[i].childNodes[1].firstChild.nodeValue+'|Practise Language:'+threads[i].childNodes[2].firstChild.nodeValue+'</p>'+
+        	        								'<p class="description">Short Description:<br/>'+threads[i].childNodes[4].firstChild.nodeValue+'</p>'+
+        													'<span class="time">Topic:'+threads[i].childNodes[3].firstChild.nodeValue+'</span>'+'</div>');
 			}
 		}
 	});
